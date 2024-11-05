91d388d4efb2a25dddabbf2de464a96d80078653
andrelui5886 <114596946+andrelui5886@users.noreply.github.com>
2024-11-05 07:43:01 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,65 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Progamação',
+    'O que é uma variável em programação??',
+    'É um espaço na memória para armazenar dados.'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Progamação',
+    'Qual a diferença entre uma linguagem de programação compilada e uma interpretada?',
+    'Linguagens compiladas geram código executável, enquanto interpretadas são executadas linha por linha.'
+)
+
+criaCartao(
+    'Matemática',
+    'Como se calcula a área de um triângulo?',
+    'A área de um triângulo qualquer pode ser calculada, multiplicando a base pela sua altura e dividindo esse resultado por 2.'
+)
+criaCartao(
+    'Progamação',
+    'O que significa a sigla "API"?',
+    'Conjunto de regras que permite que programas se comuniquem.'
+)
+criaCartao(
+    'Progamação',
+    'O que é um loop "for" e como ele funciona?',
+    'Repetição de código um número específico de vezes.'
+)
+criaCartao(
+    'Progamação',
+    'O que é uma função em programação?',
+    'Bloco de código que realiza uma tarefa e pode ser reutilizado.'
+)
+criaCartao(
+    'Progamação',
+    'O que são estruturas condicionais, como o "if" e o "else"?',
+    'São usadas para tomar decisões no código (if, else).'
+)
+criaCartao(
+    'Progamação',
+    'O que é um array (ou vetor)?',
+    'Estrutura que armazena uma coleção de dados em uma sequência ordenada.'
+)
+criaCartao(
+    'Progamação',
+    'O que significa "debugar" um código?',
+    'Identificar e corrigir erros no código.'
+)
+criaCartao(
+    'Progamação',
+    'O que é uma classe em programação orientada a objetos?',
+    'Modelo de objetos com atributos e métodos na programação orientada a objetos.'
+)
+
